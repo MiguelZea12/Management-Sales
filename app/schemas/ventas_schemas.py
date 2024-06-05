0f780257d5ed0cc4991f101a7d0bc63c4128e6a4
Miguelzea12 <aroryt02@gmail.com>
2024-06-05 14:25:44 -0500

    Templates

--- app/schemas/ventas_schemas.py
+++ app/schemas/ventas_schemas.py
@@ -5,9 +5,7 @@ from app.schemas.base_schema import BaseSchema
 class SaleSchemas (BaseSchema):
     sale_id = fields.Int(dump_only=True)
     client_id = fields.Int()
-    product_id = fields.Int()
-    quantity = fields.Int()
-    total = fields.Float()
+    datte = fields.DateTime()
     status = fields.Boolean()
     user_creation_id = fields.Integer()
     user_modification_id = fields.Integer()
